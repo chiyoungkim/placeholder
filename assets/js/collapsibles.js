f204062ce38588e83489a00e7bd0a57e40b69481
chiyoungkim <chiyoungkim@college.harvard.edu>
2017-02-10 20:27:30 -0500

    prettier html and some styling

---
+++ assets/js/collapsibles.js
@@ -0,0 +1,11 @@
+/**
+ * Created by chiyo on 2/10/2017.
+ */
+$(window).load(function () {
+    $("#pf-section-1").collapse({
+        toggle: true
+    });
+    $("#pf-section-2").collapse({
+        toggle: true
+    });
+});
---
+++ assets/js/typed-content.js
@@ -0,0 +1,33 @@
+/**
+ * Created by chiyo on 2/10/2017.
+ */
+$(function(){
+    $("#typed").typed({
+        strings: ["Chiyoung <strong>Kim</strong>"],
+        typeSpeed: 30,
+        callback: function(){
+            return elem.siblings('.typed-cursor').remove();
+        }
+    });
+});
+function shift(){
+    $(".head-wrap").addClass("shift-text");
+    terminalHeight();
+}
+
+function terminalHeight(){
+    var termHeight = $(".terminal-height");
+    var value = termHeight.text();
+    value = parseInt(value);
+    setTimeout(function(){
+        if (value > 10){
+            value = value-1;
+            this.txtValue = value.toString();
+            termHeight.text(this.txtValue);
+            self.terminalHeight();
+        }
+        else{
+            clearTimeout();
+        }
+    }, 10);
+}
