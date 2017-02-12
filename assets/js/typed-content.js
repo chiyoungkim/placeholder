97238052ebc84ab882e7ffea5fa888c91f1413d6
chiyoungkim <chiyoungkim@college.harvard.edu>
2017-02-12 04:31:20 -0500

    CLEANING UP MORE JS

--- assets/js/typed-content.js
+++ assets/js/typed-content.js
@@ -4,31 +4,6 @@
 $(function () {
     $("#typed").typed({
         strings: ["Chiyoung <strong>Kim</strong>"],
-        typeSpeed: 30,
-        callback: function () {
-            return elem.siblings('.typed-cursor').remove();
-        }
+        typeSpeed: 30
     });
 });
-
-function shift(){
-    $(".head-wrap").addClass("shift-text");
-    terminalHeight();
-}
-
-function terminalHeight(){
-    var termHeight = $(".terminal-height");
-    var value = termHeight.text();
-    value = parseInt(value);
-    setTimeout(function(){
-        if (value > 10){
-            value = value-1;
-            this.txtValue = value.toString();
-            termHeight.text(this.txtValue);
-            self.terminalHeight();
-        }
-        else{
-            clearTimeout();
-        }
-    }, 10);
-}
