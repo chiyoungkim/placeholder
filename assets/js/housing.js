4eef614653fe0d96726394da6fe52f0ffb10ca54
Chiyoung Kim <chiyoungkim@college.harvard.edu>
2017-06-20 15:23:47 -0400

    updated to read on weekly basis, not daily basis

--- assets/js/housing.js
+++ assets/js/housing.js
@@ -6,9 +6,18 @@ returnDate = function() {
     var d = new Date();
     var month = d.getMonth() + 1;
     var day = d.getDate();
-    var output = ((''+month).length<2 ? '0' : '') + month + '/' + ((''+day).length<2 ? '0' : '') + day + '/' + d.getFullYear();
+    return ((''+month).length<2 ? '0' : '') + month + '/' + ((''+day).length<2 ? '0' : '') + day + '/' + d.getFullYear();
+};
 
-    return output;
+returnWeek = function() {
+    var curr = new Date();
+    var first = curr.getDate() - curr.getDay() + 1;
+
+    var firstDay = new Date(curr.setDate(first));
+
+    var month = firstDay.getMonth() + 1;
+    var day = firstDay.getDate();
+    return ((''+month).length<2 ? '0' : '') + month + '/' + ((''+day).length<2 ? '0' : '') + day + '/' + firstDay.getFullYear();
 };
 
 returnChores = function() {
@@ -19,23 +28,37 @@ returnChores = function() {
 };
 
 tableGen = function(data) {
+
+    var currentData = {'  ':'Chores Not Found'};
+    for (datum in data) {
+        if (data[datum]['Date'] == returnWeek()) {
+            currentData = data[datum];
+            break;
+        }
+    }
+
+    data = currentData;
+
+    data['Date']=returnWeek();
+
     keys = Object.keys(data);
 
-    var heading = "<p class='center-margin' style='color:gray; '> Date: " + data[keys[0]] + "</p>";
+    var heading = "<p class='center-margin' style='color:gray; '> Date: " + returnDate() + "</p>" + "</br><p class='center-margin' style='color:gray; '>Week of: " + returnWeek() + "</p>";
 
     var myTable= "<table class=\"center-margin\"><tr><td style='width: 100px; color: #ffd42a; border-bottom:1px solid #ffd42a;'>Chore</td>";
     myTable+= "<td style='width: 100px; color: #ffd42a; text-align: right; border-bottom:1px solid #ffd42a;'>Assignee</td>";
 
+    var index = keys.indexOf('Date');
+    keys.splice(index,1);
+
     for (key in keys) {
-        if (key > 0){
-            if (key%2 == 0) {
-                myTable+="<tr style='color: gray; border-bottom:1px solid gray;'><td style='width: 100px;'>" + keys[key] + ":</td>";
-            }
-            else {
-                myTable+="<tr style='color: gray; border-bottom:1px solid gray; background-color: #ffeeaa;'><td style='width: 100px;'>" + keys[key] + ":</td>";
-            }
-            myTable+="<td style='width: 100px; text-align: right;'>" + data[keys[key]] + "</td></tr>";
+        if (key%2 == 0) {
+            myTable+="<tr style='color: gray; border-bottom:1px solid gray;'><td style='width: 100px;'>" + keys[key] + ":</td>";
         }
+        else {
+            myTable+="<tr style='color: gray; border-bottom:1px solid gray; background-color: #ffeeaa;'><td style='width: 100px;'>" + keys[key] + ":</td>";
+        }
+        myTable+="<td style='width: 100px; text-align: right;'>" + data[keys[key]] + "</td></tr>";
     }
     myTable+="</table>";
 
